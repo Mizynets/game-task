a1069fa680fe416ab17299c7bc3de566d09f4949
Sanchez <smizinec@gmail.com>
2019-09-17 13:07:10 +0300

    Updates

---
+++ precache-manifest.82b6d9378a19fdcf2a693c0348483069.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11cc22042a77f6ebc6c4a39fc76a8339",
+    "url": "/game-task/index.html"
+  },
+  {
+    "revision": "88511947b20f274dc6ef",
+    "url": "/game-task/static/css/main.fb7be0b6.chunk.css"
+  },
+  {
+    "revision": "f98006b2e03a835d70f1",
+    "url": "/game-task/static/js/2.4eda28d4.chunk.js"
+  },
+  {
+    "revision": "88511947b20f274dc6ef",
+    "url": "/game-task/static/js/main.5cd0437f.chunk.js"
+  },
+  {
+    "revision": "dd80a6d8d8f80fabef97",
+    "url": "/game-task/static/js/runtime~main.cf3c792a.js"
+  }
+]);
